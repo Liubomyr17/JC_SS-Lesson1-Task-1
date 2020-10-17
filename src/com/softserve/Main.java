78002f65511ee0f681e2e84ac0d73491e3611d86
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-17 15:09:19 +0300

    Add files via upload

---
+++ src/com/softserve/Main.java
@@ -0,0 +1,40 @@
+package com.softserve;
+
+import java.util.*;
+
+public class Main {
+        public static void main(String[] args) {
+
+            // Solution 1
+            System.out.println("Hello World!");
+
+            // Solution 2
+            System.out.println("Hello " + "World!");
+
+            // Solution 3
+            String s = "Hello ";
+            System.out.println(s + "World!");
+
+            // Solution 4
+            String s1 = "Hello";
+            String s2 = " World!";
+            System.out.println(s1 + s2);
+
+            // Solution 5
+            String str1 = "Hello";
+            String str2 = "World!";
+            System.out.println(str1 + " " + str2);
+
+            // Solution 6
+            String string = "Hello World!";
+            System.out.println(string);
+
+            // Solution 7
+            System.out.print("Hello ");
+            System.out.println("World!");
+
+        }
+}
+
+
+
